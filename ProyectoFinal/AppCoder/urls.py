f219c73e4d7a4bdcc475c8e30db45dd8bee0f1b9
Carlos Tavares <carlos.tavaresa@outlook.com>
2023-11-28 13:29:41 -0400

    trabajo de rutas

--- ProyectoFinal/AppCoder/urls.py
+++ ProyectoFinal/AppCoder/urls.py
@@ -1,5 +1,13 @@
 from django.urls import path
+from django.http import HttpResponse
+
+def view_inicio(request):
+    return HttpResponse("Bienvenidos")
+
+def view_cursos(request):
+    return HttpResponse("Aquí mostraré los cursos")
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('inicio/', view_inicio),
+    path('cursos/', view_cursos)
 ]
